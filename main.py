b66887e4b6fd8c2f956f354086937c20a4867e11
stevestatia <steve.statia@gmail.com>
2021-02-01 02:50:45 -0800

    Added a lot of stuff still pre alpha

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests, urllib3, os
 from tkinter import *
+from tkinter.ttk import *
+from time import sleep
 from bs4 import BeautifulSoup
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -15,7 +17,9 @@ urllib3.disable_warnings()
 res_information = []
 res_sizes = []
 wallpaper_links = []
+selected_resolutions = []
 wallpaper_errors = []
+progressbar_position = 0
 pageUrl = "http://interfacelift.com/wallpaper/downloads/date/any/index"  # + pageNUmber + ".html"
 urlSuffix = ".html"
 
@@ -60,6 +64,7 @@ def download_wallpaper_to_file(wallpaper_url, pictureInfo):
 
 def scrap_pages_for_wallpaper_urls():
     for pageNum in range(1, 394):
+        progressbar_position = pageNum
         page_num_as_string = str(pageNum)
         new_url = pageUrl + page_num_as_string + urlSuffix
 
@@ -79,6 +84,7 @@ def scrap_pages_for_wallpaper_urls():
 
                 wallpaper_links.append(build_image_url_without_res(picture_info[0], picture_info[1]))
             print("Page " + page_num_as_string + " complete.")
+    progressbar_position = 0
 
 
 def save_wallpaper_link_to_file():
@@ -87,17 +93,78 @@ def save_wallpaper_link_to_file():
             text_file.write("".join(index) + "\n")
 
 
-def generate_gui():
+def gui():
     tk = Tk()
     tk.title('InterfaceLIFT Bulk Wallpaper Downloader')
+    tk.geometry("400x300")
+    tk.resizable(False, False)
 
-    frame = Frame(master=tk, width=500, height=500)
-    frame.pack()
 
-    yes = StringVar(tk)
-    yes.set("Please select your resolution")
-    res_dropdown = OptionMenu(tk, yes, *res_information)
-    res_dropdown.pack()
+    # Drop down resolution selection menu
+    # option_menu_selection = StringVar(tk)
+    # option_menu_selection.set(res_information[0])
+    # res_dropdown = OptionMenu(tk, option_menu_selection, *res_information)
+    # res_dropdown.pack(side=BOTTOM)
+
+    selected_resolutions_list_box = Listbox(tk, width=40, height=10)
+    selected_resolutions_list_box.grid(column=2, row=1)
+
+    res_dropdown = Combobox(tk, state="readonly")
+    res_dropdown['values'] = res_information
+    res_dropdown.current(0)
+    res_dropdown.grid(column=2, row=2)
+
+    def add_res_to_list():
+        if res_dropdown.get() in selected_resolutions:
+            print("Resolution is already selected.")
+        else:
+            selected_resolutions.append(res_dropdown.get())
+            selected_resolutions_list_box.insert('anchor', res_dropdown.get())
+            print(selected_resolutions)
+
+    # Button to add resolutions to the list of wallpaper downloads
+    add_button = Button(master=tk, text="Add", command=add_res_to_list)
+    add_button.grid(column=1, row=3)
+
+    def remove_res_from_list():
+        try:
+            selected_resolutions.remove(selected_resolutions_list_box.get(selected_resolutions_list_box.curselection()))
+            selected_resolutions_list_box.delete(selected_resolutions_list_box.curselection())
+        except TclError:
+            print("Nothing selected")
+
+    remove_button = Button(master=tk, text="Remove", command=remove_res_from_list)
+    remove_button.grid(column=3, row=3)
+
+    def clear_list():
+        selected_resolutions.clear()
+        selected_resolutions_list_box.delete(0, END)
+
+    clear_button = Button(master=tk, text="Clear List", command=clear_list)
+    clear_button.grid(column=3, row=4)
+
+    def refresh_command():
+        teams = range(10)
+        popup = Toplevel()
+        popup.grab_set()
+        Label(popup, text="Files being downloaded").grid(row=0, column=0)
+
+        progress = 0
+        progress_var = DoubleVar()
+        progress_bar = Progressbar(popup, variable=progress_var, maximum=100)
+        progress_bar.grid(row=1, column=0)  # .pack(fill=tk.X, expand=1, side=tk.BOTTOM)
+        popup.pack_slaves()
+
+        progress_step = float(100.0 / len(teams))
+        for team in teams:
+            popup.update()
+            sleep(1)  # lauch task
+            progress += progress_step
+            progress_var.set(progress)
+        popup.destroy()
+
+    fetch_urls_button = Button(master=tk, text="Refresh", command=refresh_command)
+    fetch_urls_button.grid(column=2, row=5)
 
     tk.mainloop()
 
@@ -113,7 +180,7 @@ def read_data_from_file():
 
 def main():
     read_data_from_file()
-    generate_gui()
+    gui()
     if os.path.exists("wallpaper_links_without_res.txt"):
         return True
 
